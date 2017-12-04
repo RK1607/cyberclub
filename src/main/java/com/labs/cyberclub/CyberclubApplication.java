32efb2932a9192192947746c5a366a0f6244b25a
Ruslan <kutsenkoruslan1607@gmail.com>
2017-12-04 20:42:14 +0200

    Настроили сервер

---
+++ src/main/java/com/labs/cyberclub/CyberclubApplication.java
@@ -0,0 +1,12 @@
+package com.labs.cyberclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberclubApplication.class, args);
+	}
+}
